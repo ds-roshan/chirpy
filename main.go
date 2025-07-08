ffca9573bf041cc0dbf8faa2582f1c8a64c785cb
Roshan <ds.roshanlamichhane@gmail.com>
2025-07-08 16:14:23 +0545

    Add metrics, readiness, and reset endpoints with hit counter

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-
-	serveMux.HandleFunc("/healthz", handler)
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	serveMux.HandleFunc("GET /api/healthz", handlerReadiness)
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -25,8 +34,9 @@ func main() {
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
